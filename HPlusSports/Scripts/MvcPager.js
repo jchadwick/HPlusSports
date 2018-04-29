88497db71ed4933956c64d44d8ca4a65512f807e
Jess Chadwick <jesschadwick@gmail.com>
2018-04-29 01:00:01 -0400

    Fixing JavaScripts

--- HPlusSports/Scripts/MvcPager.js
+++ HPlusSports/Scripts/MvcPager.js
@@ -1,12 +1,9 @@
 ﻿$(function () {
   var pagedContent = $('.paged-content');
-
   pagedContent.on('click', '[data-page-button]', function (evt) {
-    evt.stopPropagation();
     var url = $(evt.currentTarget).attr('href');
-    console.debug('Navigating to page ' + url);
     pagedContent.load(url);
-    return false;
+    evt.preventDefault();
+    evt.stopPropagation();
   });
-
 });
